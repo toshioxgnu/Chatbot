ff3dabde46a8d9c2338592ee5866a9f0310c7330
toshioxgnu <jozho.g@gmail.com>
2019-09-10 17:17:04 -0300

    Testeando para twitter los commits

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent["tag"])
         if(intent["tag"])not in labels:
             labels.append(intent["tag"])
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
 training =[]
 output = []
-out_empty=[0 for _ in range(len(classes))]
+out_empty=[0 for _ in range(len(labels))]
 for doc in ennumerate(docs_x):
     bag = []
     wrds = [stemmer.stem(w) for w in doc]
@@ -46,3 +46,6 @@ for doc in ennumerate(docs_x):
     output.append(output_row)
 training = numpy.array(training)
 output = np.array(output)
+
+training=numpy.array(training)
+output = numpy.array(output)
