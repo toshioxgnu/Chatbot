41c7d8999f64b3885a88e5063861a95d2ddb2a3b
Jose Gonzalez <jozho.g@gmail.com>
2019-09-16 21:54:56 -0300

    Arreglando el entrenamiento en el chatbot

--- main.py
+++ main.py
@@ -7,21 +7,20 @@ import tflearn
 import tensorflow
 import random
 import json
-import pickle
+# import pickle
 
 with open("intents.json") as file:
     data=json.load(file)
 
 
-try:
-    with open("data.pickle", "rb") as f:
-        words,labels, training, output = picke.load(f)
-except():
-    words=[]
-    labels=[]
-    docs_y=[]
-    docs_x=[]
-    for intent in data["intents"]:
+
+
+
+words=[]
+labels=[]
+docs_y=[]
+docs_x=[]
+for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
@@ -29,36 +28,35 @@ except():
         docs_y.append(intent["tag"])
         if(intent["tag"])not in labels:
             labels.append(intent["tag"])
-    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
-    words = sorted(list(set(words)))
+words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+words = sorted(list(set(words)))
 
-    labels = sorted(labels)
-    training =[]
-    output = []
-    out_empty=[0 for _ in range(len(labels))]
-    for x, doc in enumerate(docs_x):
+labels = sorted(labels)
+training =[]
+output = []
+out_empty=[0 for _ in range(len(labels))]
+for x, doc in enumerate(docs_x):
     bag = []
-    wrds = [stemmer.stem(w) for w in doc]
+wrds = [stemmer.stem(w) for w in doc]
 
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
+for w in words:
+    if w in wrds:
+        bag.append(1)
+    else:
+        bag.append(0)
+output_row = out_empty[:]
+output_row[labels.index(docs_y[x])] = 1
 
-    training.append(bag)
-    output.append(output_row)
+training.append(bag)
+output.append(output_row)
 
-    training = numpy.array(training)
-    output = numpy.array(output)
+training = numpy.array(training)
+output = numpy.array(output)
 
-    training=numpy.array(training)
-    output = numpy.array(output)
+training=numpy.array(training)
+output = numpy.array(output)
 
-    with open("data.pickle", "wb") as f:
-        pickle.dump((words,labels, training, output), f)
+
 
 
 # red neural y sus capas
